81e8375bf1bb3649092ab6ef4a94a5d20d505a60
Bruna Bazaluk M. V <bruna.videira@usp.br>
2021-11-03 15:53:41 -0300

    Gera variaveis "default".
    Signed-off-by: Bruna Bazaluk M. V <bruna.videira@usp.br>

--- birdscapes/src/components/Birdscapes.vue
+++ birdscapes/src/components/Birdscapes.vue
@@ -4,46 +4,64 @@
 	<h1> BIRDSCAPES</h1>
 	</div>
 
-<fieldset>  
-  <legend>Horários</legend>
+	<fieldset>  
+		<legend>Horários</legend>
 
-  <input type="radio" id="notify-on" name="notify" value="on">
-  <label >amanhecer</label>
-  
-  <input type="radio" id="notify-on" name="notify" value="on">
-  <label>dia</label>
-  
-  <input type="radio" id="notify-on" name="notify" value="on">
-  <label>entardecer</label>
+		<input v-model="time" type="radio" value="6:00">
+		<label >amanhecer</label>
 
-  <input type="radio" id="notify-on" name="notify" value="on">
-  <label>noite</label>
+		<input v-model="time" type="radio" value="10:00">
+		<label>dia</label>
 
-</fieldset>
+		<input v-model="time" type="radio" value="16:00">
+		<label>entardecer</label>
 
-<fieldset>  
-  <legend>Estação do ano</legend>
+		<input v-model="time" type="radio" value="19:00">
+		<label>noite</label>
+	</fieldset>
 
-  <input type="radio" id="notify-on" name="notify" value="on">
-  <label >primavera</label>
-  
-  <input type="radio" id="notify-on" name="notify" value="on">
-  <label >verão</label>
-  
-  <input type="radio" id="notify-on" name="notify" value="on">
-  <label>outono</label>
-  
-  <input type="radio" id="notify-on" name="notify" value="on">
-  <label>inverno</label>
+	<fieldset>  
+		<legend>Estação do ano</legend>
 
-</fieldset>
+		<input v-model="season" type="radio" id="notify-on" name="notify" value="primavera">
+		<label >primavera</label>
 
+		<input v-model="season" type="radio" id="notify-on" name="notify" value="verao">
+		<label >verão</label>
+
+		<input v-model="season" type="radio" id="notify-on" name="notify" value="outono">
+		<label>outono</label>
+
+		<input v-model="season" type="radio" id="notify-on" name="notify" value="inverno">
+		<label>inverno</label>
+	</fieldset>
+	
+	<p>{{ time }}</p>
 
 </template>
 
-<script>
-</script>
 
+
+
+<script>
+
+
+	export default { 
+		data() {
+			return {
+				time: '10:00',
+				season: 'primavera',
+				position: '100',
+				imgs: [],
+				popular: [],
+				latim: [],
+				ingles: [],
+				audio_passaro: ''
+			}
+		}
+	}
+
+</script>
 
 
 
