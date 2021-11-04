90687264dd4f099cbe4c1e6547ec8e8467ba7fc6
Bruna Bazaluk M. V <bruna.videira@usp.br>
2021-11-04 13:53:13 -0300

    Organiza pastas.
    Signed-off-by: Bruna Bazaluk M. V <bruna.videira@usp.br>

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# birdscapes-front
+# birdscapes
+
+
+### Ambiente virtual
+Assim que baixar o repositorio entre no ambiente virtual, caso esteja no Linux:
+
+``` $  source venv/bin/activate```
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/Birdscapes.vue
@@ -0,0 +1,79 @@
+<template>
+
+	<div>
+	<h1> BIRDSCAPES</h1>
+	</div>
+
+	<fieldset>  
+		<legend>Horários</legend>
+
+		<input v-model="time" type="radio" value="6:00">
+		<label >amanhecer</label>
+
+		<input v-model="time" type="radio" value="10:00">
+		<label>dia</label>
+
+		<input v-model="time" type="radio" value="16:00">
+		<label>entardecer</label>
+
+		<input v-model="time" type="radio" value="19:00">
+		<label>noite</label>
+	</fieldset>
+
+	<fieldset>  
+		<legend>Estação do ano</legend>
+
+		<input v-model="season" type="radio" id="notify-on" name="notify" value="primavera">
+		<label >primavera</label>
+
+		<input v-model="season" type="radio" id="notify-on" name="notify" value="verao">
+		<label >verão</label>
+
+		<input v-model="season" type="radio" id="notify-on" name="notify" value="outono">
+		<label>outono</label>
+
+		<input v-model="season" type="radio" id="notify-on" name="notify" value="inverno">
+		<label>inverno</label>
+	</fieldset>
+	
+	<p>{{ time }}</p>
+
+</template>
+
+
+
+
+<script>
+
+	var time;
+	var season;
+	var position;
+
+	export default { 
+		data() {
+			return {
+				time: '10:00',
+				season: 'primavera',
+				position: '100',
+				imgs: [],
+				popular: [],
+				latim: [],
+				ingles: [],
+				audio_passaro: ''
+			}
+		}
+	}
+
+	var obj = new Object();
+	obj.time = time;
+	obj.season  = season;
+	obj.position = position;
+	//var json_obj= JSON.stringify(obj);
+
+
+</script>
+
+
+
+<style>
+</style>
